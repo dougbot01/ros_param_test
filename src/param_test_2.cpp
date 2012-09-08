db4dcaf024ad29eaa491a5b083f67731c0981fd5
Doug Sievers <dwsievers@gmail.com>
2012-09-07 19:43:26 -0700

    first commit

---
+++ src/param_test_2.cpp
@@ -0,0 +1,26 @@
+#include <ros/ros.h>
+
+int main(int argc, char* argv[])
+{
+    int serial_number = -1;
+
+    ros::init(argc, argv, "parameter_test");
+
+    //ros::NodeHandle n;
+    //n.getParam("serial", serial_number);
+    ros::NodeHandle nh("~");
+    nh.getParam("serial", serial_number);
+    //nh.getParam("/serial", serial_number);
+
+    ros::Rate loop_rate(10);
+
+    ROS_INFO("Serial was %d", serial_number);
+
+    while (ros::ok())
+    {
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+
+}
+
