f744d965495b8e37520f865533e76bb497cfcee7
Doug Sievers <dwsievers@gmail.com>
2012-09-09 20:28:49 -0700

    Rename files for clarity

--- src/param_test_global.cpp
+++ src/param_test_global.cpp
@@ -3,24 +3,18 @@
 int main(int argc, char* argv[])
 {
     int serial_number = -1;
-
     ros::init(argc, argv, "parameter_test");
 
+    //n is in the global namespace
     ros::NodeHandle n;
     n.getParam("serial", serial_number);
-    //ros::NodeHandle nh("~");
-    //nh.getParam("serial", serial_number);
-    //nh.getParam("/serial", serial_number);
 
     ros::Rate loop_rate(10);
-
     ROS_INFO("Serial was %d", serial_number);
-
     while (ros::ok())
     {
         ros::spinOnce();
         loop_rate.sleep();
     }
-
 }
 
--- src/param_test_local.cpp
+++ src/param_test_local.cpp
@@ -3,24 +3,18 @@
 int main(int argc, char* argv[])
 {
     int serial_number = -1;
-
     ros::init(argc, argv, "parameter_test");
 
-    //ros::NodeHandle n;
-    //n.getParam("serial", serial_number);
-    ros::NodeHandle nh("~");
-    nh.getParam("serial", serial_number);
-    //nh.getParam("/serial", serial_number);
+    //n is in the node's namespace
+    ros::NodeHandle n("~");
+    n.getParam("serial", serial_number);
 
     ros::Rate loop_rate(10);
-
     ROS_INFO("Serial was %d", serial_number);
-
     while (ros::ok())
     {
         ros::spinOnce();
         loop_rate.sleep();
     }
-
 }
 
